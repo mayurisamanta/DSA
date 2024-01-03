1911e45c11e5d331d87a4576a9d22200fa655cef
mayurisamanta <samantamayuri@gmail.com>
2024-01-03 21:50:06 +0530

    Convert an Array into a 2D array with conditions (solution 2)

--- leetcode/$2610_Convert_an_Array_Into_a_2D_Array_With_Conditions/Convert_an_Array_Into_a_2D_Array_With_Conditions.java
+++ leetcode/$2610_Convert_an_Array_Into_a_2D_Array_With_Conditions/Convert_an_Array_Into_a_2D_Array_With_Conditions.java
@@ -1,11 +1,41 @@
 package leetcode.$2610_Convert_an_Array_Into_a_2D_Array_With_Conditions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Convert_an_Array_Into_a_2D_Array_With_Conditions {
+
+    public static List<List<Integer>> convertTo2DArray2(int[] nums){
+
+        List<List<Integer>> result = new ArrayList<>();
+        int[] count = new int[nums.length + 1];
+
+        for (int num : nums){
+            count[num]++;
+
+            if (count[num] > result.size()){
+                result.add(new ArrayList<>());
+            }
+
+            result.get(count[num] - 1).add(num);
+        }
+
+        return result;
+    }
+
+    /*
+    Approach 2: using count array
+    We have already given Constraints
+    1 <= nums.length <= 200
+    1 <= nums[i] <= nums.length
+    So we can create a count array of size nums.length + 1
+    and store the count of each element in the count array
+    then if count of any element is greater than the size of result list
+    then we can add a new list in the result list
+    and add the element in the list at index count[num] - 1
+     */
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
 
     public static List<List<Integer>>  convertTo2DArray(int[] nums) {
         int max = 0;
@@ -36,6 +66,7 @@ public class Convert_an_Array_Into_a_2D_Array_With_Conditions {
     public static void main(String[] args) {
         int[] nums = {1,3,4,1,2,3,1};
         System.out.println(convertTo2DArray(nums));
+        System.out.println(convertTo2DArray2(nums));
     }
 
     // Time Complexity: O(n)
